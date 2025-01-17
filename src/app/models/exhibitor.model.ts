2cb461950c6b3469d1ca4ad2388bfebaeaf6eb97
Khairus <playsave23@gmail.com>
2025-01-17 10:05:25 +0700

    feat: add exhibitor model with company, exhibitor, and addExhibitorResponse interface

---
+++ src/app/models/exhibitor.model.ts
@@ -0,0 +1,23 @@
+export interface Company {
+  S_company: string;
+  S_event: string;
+}
+
+export interface Exhibitor {
+  S_added_via: string;
+  S_company: string;
+  S_email_address: string;
+  S_group_reg_id: string;
+  S_name_on_badge: string;
+  S_job_title: string;
+  S_country: string;
+  S_company_on_badge: string;
+  SB_event_fha: boolean;
+  SB_event_prowine: boolean;
+}
+
+export interface AddExhibitorResponse {
+  status: boolean;
+  message: string;
+  user_id?: string;
+}
