ff72d197ca35ee70cdfb75f7838d07cea860e79e
Khairus <playsave23@gmail.com>
2025-01-20 10:34:55 +0700

    feat: implement loading overlay and integrate service in exhibitors-registration

--- src/app/pages/exhibitors-registration/exhibitors-registration.component.ts
+++ src/app/pages/exhibitors-registration/exhibitors-registration.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BannerComponent } from '../../components/banner/banner.component';
+import { ExhibitorFormComponent } from '../../components/exhibitor-form/exhibitor-form.component';
+import { ExhibitorService } from '../../services/exhibitor.service';
+import { Company } from '../../models/exhibitor.model';
+import { finalize } from 'rxjs';
+import { LoadingOverlayComponent } from '../../components/loading-overlay/loading-overlay.component';
 
 @Component({
   selector: 'app-exhibitors-registration',
-  imports: [],
+  standalone: true,
+  imports: [BannerComponent, ExhibitorFormComponent, LoadingOverlayComponent],
   templateUrl: './exhibitors-registration.component.html',
-  styleUrl: './exhibitors-registration.component.scss'
+  styleUrl: './exhibitors-registration.component.scss',
 })
-export class ExhibitorsRegistrationComponent {
+export class ExhibitorsRegistrationComponent implements OnInit {
+  private exhibitorService = inject(ExhibitorService);
 
+  public isLoading = false;
+  public companies: Company[] = [];
+  public countries: string[] = [];
+  public selectedEvent: string = '';
+
+  ngOnInit(): void {
+    this.loadCompanies();
+    this.fetchCountries();
+  }
+
+  loadCompanies(): void {
+    this.isLoading = true;
+    this.exhibitorService
+      .getCompanies({})
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe((data) => {
+        this.companies = data?.message;
+      });
+  }
+
+  fetchCountries() {
+    this.isLoading = true;
+
+    this.exhibitorService
+      .getCountries()
+      .pipe(finalize(() => (this.isLoading = false)))
+      .subscribe(
+        (data: { country: string }[]) => {
+          this.countries = [...new Set(data.map((item: any) => item.country))];
+        },
+        (error) => {
+          console.error('Failed to fetch countries', error);
+        }
+      );
+  }
 }
